c2607ee5df9c7ba1dad256c92850bf0174e8f809
Alain Ravet <alainravet@gmail.com>
2012-03-16 09:15:57 +0100

    add `rake test`  + only require the RubyMine's specific gem when launching the test in RubyMine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,13 @@ require_relative '../lib/calculator'
 require 'minitest/spec'
 require 'minitest/autorun'
 
+
 # for Rubymine (optional)
-require 'minitest/reporters'
-MiniTest::Unit.runner = MiniTest::SuiteRunner.new
-MiniTest::Unit.runner.reporters << MiniTest::Reporters::RubyMineReporter.new
+if $0 =~ /RubyMine\.app/
+  require 'minitest/reporters'
+  MiniTest::Unit.runner = MiniTest::SuiteRunner.new
+  MiniTest::Unit.runner.reporters << MiniTest::Reporters::RubyMineReporter.new
+end
 
 def tbi(_int_value)
   TwoByteInt.new(_int_value)
